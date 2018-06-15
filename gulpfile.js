4a8eb106aed360a0390eab2938c8eb305ba25a8b
Castro <cassio.castro@accenture.com>
2018-06-15 11:29:10 -0300

    Adicionando o modulo browser-sync ao projeto e adicionando a task server ao gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+//==== SASS ====//
 
 // Task para processamento do SASS
 gulp.task('sass', function(){
@@ -13,7 +16,28 @@ gulp.task('sass', function(){
 
 
 // Task para monitoramento do SASS
-gulp.task('listen', function(){
-                // PATH  - COMANDO A SER EXECUTADO 
+// gulp.task('listen', function(){
+//                 // PATH  - COMANDO A SER EXECUTADO 
+//     gulp.watch('./src/sass/**/*.scss', ['sass'])
+// })
+
+
+//==== BROWSER SYNC ====//
+
+// Task para iniciar um servidor localhost
+gulp.task('server', function(){
+    // Modulo.metodo
+    browserSync.init({
+        // Objeto
+        server: {
+            // Diretorio a ser servido
+            baseDir: 'src'
+        }
+    })
+
+    // Monitoramento do CSS com reload no browser
+    gulp.watch('src/css/*.css').on('change', browserSync.reload)
+    
+    // Monitoramento do SASS
     gulp.watch('./src/sass/**/*.scss', ['sass'])
 })
